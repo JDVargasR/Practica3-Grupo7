7789ee2abe747bbded6d1dd0d359b665c4aacb3e
Abraham3105 <acascantebadilla31@gmail.com>
2025-03-17 15:37:30 -0600

    Arreglo abraham

--- src/main/java/com/practica01/Practica01Application.java
+++ src/main/java/com/practica01/Practica01Application.java
@@ -8,6 +8,9 @@ public class Practica01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica01Application.class, args);
+                
+                
+                
 	}
 
 }
